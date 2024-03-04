acbc3118dce5dbab3ae6ca6b3e716d76f36a3901
Tristan Murphy <TristanMurphy@marsk12.org>
2024-03-04 09:08:14 -0500

    camera settings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
+use bevy::core_pipeline::bloom::BloomSettings;
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::*;
 
 fn main() {
     App::new()
-        .with_plugins((
-            DefaultPlugins,
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "CreatePT Game".to_string(),
+                    ..default()
+                }),
+                ..default()
+            }),
             PhysicsPlugins::default(),
         ))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-) {
-    commands.spawn(Camera3dBundle::default());
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            ..default()
+        },
+         BloomSettings::NATURAL
+    ));
 }
